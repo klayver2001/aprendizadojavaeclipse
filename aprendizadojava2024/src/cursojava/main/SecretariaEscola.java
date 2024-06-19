1ae2b8cb716fa40384e9f95d4686fca3cafd9ebf
klayver2001 <klayveralencar2001@gmail.com>
2024-06-19 15:48:18 -0300

    :books: Loop FOR e "trocarAluno", para remover e adicionar novo aluno

--- aprendizadojava2024/src/cursojava/main/SecretariaEscola.java
+++ aprendizadojava2024/src/cursojava/main/SecretariaEscola.java
@@ -2,7 +2,6 @@ package cursojava.main;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.JOptionPane;
 import cursojava.classes.Aluno;
 import cursojava.classes.Disciplina;
@@ -10,11 +9,13 @@ import cursojava.classes.Disciplina;
 public class SecretariaEscola {
 
 	public static void main(String[] args) {
-
+		// Criei uma lista para armazenar os objetos Aluno
 		List<Aluno> alunos = new ArrayList<Aluno>();
 
+		/* For para cadastro de até 2 alunos */
 		for (int quantidade = 1; quantidade <= 2; quantidade++) {
 
+			/*  */
 			String nome = JOptionPane.showInputDialog("Nome do aluno : " + quantidade + " ?");
 			/*
 			 * String idade = JOptionPane.showInputDialog("Idade do Aluno : "); String
@@ -25,8 +26,8 @@ public class SecretariaEscola {
 			 * JOptionPane.showInputDialog("Nome do Pai : "); String MatriculaId =
 			 * JOptionPane.showInputDialog("Número da Matrícula : ");
 			 */
-			// Cria instâncias dos alunos
 
+			// Instâncias de alunos
 			Aluno aluno1 = new Aluno();
 			aluno1.setNome(nome);
 			/*
@@ -36,7 +37,7 @@ public class SecretariaEscola {
 			 * aluno1.setNomeMae(NomeMae); aluno1.setNomePai(NomePai);
 			 * aluno1.setMatriculaId(MatriculaId);
 			 */
-			for (int pos = 1; pos <= 4; pos++) {
+			for (int pos = 1; pos <= 1; pos++) {
 				String nomeDisciplina = JOptionPane.showInputDialog("Nome da Disciplina " + pos + " : ");
 				String notaDisciplina = JOptionPane.showInputDialog("Nota da Disciplina " + pos + " : ");
 				Disciplina disciplina = new Disciplina();
@@ -60,16 +61,32 @@ public class SecretariaEscola {
 		}
 		for (int pos = 0; pos < alunos.size(); pos++) {
 			Aluno aluno = alunos.get(pos);
+
+			if (aluno.getNome().equalsIgnoreCase("klayver")) {
+				Aluno trocarAluno = new Aluno();
+				trocarAluno.setNome("Aluno trocado !");
+
+				Disciplina disciplina = new Disciplina();
+				disciplina.setDisciplina("Math");
+				disciplina.setNota(90);
+
+				trocarAluno.getDisciplinas().add(disciplina);
+
+				alunos.set(pos, trocarAluno);
+				aluno = alunos.get(pos);
+			}
+
+			System.out.println("---------------------------------------");
 			System.out.println("Aluno : " + aluno.getNome());
 			System.out.println("Média do aluno : " + aluno.getMediaNotas());
 			System.out.println("Resultado do ano letivo : " + aluno.getAlunoAprovado2());
-			System.out.println("---------------------------------------");
-			
+			System.out.println("7");
+
 			for (int posicaodisciplina = 0; posicaodisciplina < aluno.getDisciplinas().size(); posicaodisciplina++) {
 				Disciplina disc = aluno.getDisciplinas().get(posicaodisciplina);
 				System.out.println("Disciplina : " + disc.getDisciplina());
 				System.out.println("Nota : " + disc.getNota());
-				
+
 			}
 
 		}
